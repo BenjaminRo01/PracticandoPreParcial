6b87e464149da74692db50c0224cbde580697493
BenjaminRo01 <benjaroselo01@gmail.com>
2024-06-12 18:57:12 -0300

    Update CarritoCompras.java

--- src/main/java/strategy/model/CarritoCompras.java
+++ src/main/java/strategy/model/CarritoCompras.java
@@ -15,9 +15,16 @@ public class CarritoCompras {
     }
     public void agregarProducto(Producto producto){
         this.productos.add(producto);
-        this.precioCarrito = producto.precio();
+        this.actualizarPrecioCarrito(producto);
+        this.actualizarPesoCarrito(producto);
+    }
+    private void actualizarPesoCarrito(Producto producto) {
         this.pesoCarrito = producto.peso();
     }
+    private void actualizarPrecioCarrito(Producto producto) {
+        this.precioCarrito = producto.precio();
+    }
+
     public double calcularCostoTotal(Destino destino, int distancia){
         return this.precioCarrito + envio.calcularEnvio(this.pesoCarrito, destino, distancia);
     }
